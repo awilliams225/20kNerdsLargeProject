f4867f2f7ca97b5894d824cf63f0717e46ba06bb
sum244 <91293022+sum244@users.noreply.github.com>
2023-10-25 16:40:33 -0400

    Update LoginPage.js

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.css"
 
 import PageTitle from '../components/PageTitle';
 import Login from '../components/Login';
+import Register from '../components/Register';
 
-const LoginPage = () =>
-{
-
-    return(
-      <div>
-        <PageTitle />
-        <Login />
-      </div>
+const LoginPage = () => {
+    return (
+        <div>
+            <div style={{
+                backgroundColor: '#FFFFFF',
+                height: '100vh', width: '50vw', float: 'right'
+            }}>
+                <h1 style={{color: '#4BBCD5'}}>
+                    R FLIGHT</h1>
+                <Register />
+            </div>
+            <div style={{
+                backgroundColor: '#FF0000',
+                height: '100vh', width: '50vw'
+            }}>
+                <h1 style={{float: 'right', color: '#FFFFFF'}}>
+                    FIGHT O</h1>
+                <Login />
+            </div>
+        </div>
     );
 };
 

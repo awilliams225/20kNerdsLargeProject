b935ecef88e1c65d01779e4b36481304f2e2404b
sum244 <91293022+sum244@users.noreply.github.com>
2023-10-26 14:07:18 -0400

    Update Register.js

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,57 +1,80 @@
-//import React from 'react';
 import React, { useState } from 'react';
 
-function Login() {
-    var loginName;
-    var loginPassword;
+function Register()
+{
 
-    const [message, setMessage] = useState('');
+  var registerName;
+  var registerPassword;
+  var registerEmail;
 
-    const doLogin = async event => {
-        event.preventDefault();
-        //alert('doIt() ' + loginName.value + ' ' + loginPassword.value);
+  const [message,setMessage] = useState('');
 
-        var obj = { login: loginName.value, password: loginPassword.value };
-        var js = JSON.stringify(obj);
+  const app_name = 'fight-or-flight-20k-5991cb1c14ef'
+  function buildPath(route)
+  {
+    if (process.env.NODE_ENV === 'production') 
+    {
+        return 'https://' + app_name +  '.herokuapp.com/' + route;
+    }
+    else
+    {        
+        return 'http://localhost:5000/' + route;
+    }
+  }
 
-        try {
-            const response = await fetch('http://localhost:5001/api/login',
-                { method: 'POST', body: js, headers: { 'Content-Type': 'application/json' } });
+  const doRegister = async event => 
+  {
+      event.preventDefault();
 
-            var res = JSON.parse(await response.text());
+      var obj = {username:registerName.value,password:registerPassword.value,
+                 email:registerEmail.value};
+      var js = JSON.stringify(obj);
 
-            if (res.id <= 0) {
-                setMessage('User/Password combination incorrect');
-            }
-            else {
-                var user = { firstName: res.firstName, lastName: res.lastName, id: res.id }
-                localStorage.setItem('user_data', JSON.stringify(user));
+      try
+      {    
+          const response = await fetch(buildPath('api/register'),
+              {method:'POST',body:js,headers:{'Content-Type': 'application/json'}});
 
-                setMessage('');
-                window.location.href = '/cards';
-            }
-        }
-        catch (e) {
-            alert(e.toString());
-            return;
-        }
-    };
+          var res = JSON.parse(await response.text());
 
-    return (
-        <div id="LoginDiv" style={{backgroundColor: '#4BBCD5', height: '50vh', width: '75%',
-           borderRadius: '0px 15px 15px 0px', margin: '10vh 0px'}}>
-            <form onSubmit={doLogin} style={{marginTop: '40px'}}>
+          if( res.id <= 0 )
+          {
+              setMessage('User/Password combination incorrect');
+          }
+          else
+          {
+              var user = {firstName:res.firstName,lastName:res.lastName,id:res.id}
+              localStorage.setItem('user_data', JSON.stringify(user));
+
+              setMessage('');
+              window.location.href = '/cards';
+          }
+      }
+      catch(e)
+      {
+          alert(e.toString());
+          return;
+      }    
+  };
+
+    return(
+      <div id="RegisterDiv" style={{backgroundColor: '#4BBCD5', height: '50vh', width: '75%',
+           borderRadius: '0px 15px 15px 0px', float: 'left', marginTop: '10vh',
+           textAlign: 'center', padding: '5vh'}}>
+            <form onSubmit={doRegister} style={{}}>
                 <span id="inner-title" style={{color: '#FFFFFF', marginTop: '20px'}}>Sign Up</span><br />
-                <input type="text" id="loginName" placeholder="Username"
-                    ref={(c) => loginName = c} /><br />
-                <input type="password" id="loginPassword" placeholder="Password"
-                    ref={(c) => loginPassword = c} /><br />
-                <input type="submit" id="loginButton" class="buttons" value="Do It"
-                    onClick={doLogin} />
+                <input type="text" id="registerName" placeholder="Username"
+                    ref={(c) => registerName = c} /><br />
+                <input type="email" id="registerEmail" placeholder="Email"
+                    ref={(c) => registerEmail = c} /><br />
+                <input type="password" id="registerPassword" placeholder="Password"
+                    ref={(c) => registerPassword = c} /><br />
+                <input type="submit" id="lregisterButton" class="buttons" value="Create Account"
+                    onClick={doRegister} />
             </form>
-            <span id="loginResult">{message}</span>
+            <span id="registerResult">{message}</span>
         </div>
     );
 };
 
-export default Login;
+export default Register;

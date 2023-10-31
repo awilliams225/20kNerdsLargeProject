a9ca5af706b91c9381729f35c0802dbab0978057
awilliams225 <ashwilliams255@gmail.com>
2023-10-31 15:31:42 -0400

    Created post route and post component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-
+import Post from './components/Post'
 import LoginPage from './pages/LoginPage';
 import CardPage from './pages/CardPage';
 
@@ -11,9 +11,10 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/cards" element={<CardPage />} />
+        <Route path="/posts/:slug" element={<Post />}/>
       </Routes>
     </BrowserRouter>
-      );
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Post.js
@@ -0,0 +1,69 @@
+import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+
+
+export default function Post() {
+    const { slug } = useParams();
+
+    const [post, setPost] = useState({});
+    const [loading, setLoading] = useState(false);
+
+    const app_name = 'fight-or-flight-20k-5991cb1c14ef'
+    function buildPath(route)
+    {
+      if (process.env.NODE_ENV === 'production') 
+      {
+          return 'https://' + app_name +  '.herokuapp.com/' + route;
+      }
+      else
+      {        
+          return 'http://localhost:5000/' + route;
+      }
+    }
+    
+    useEffect(() => {
+        const grabPost = async () => {
+            setLoading(true);
+
+            const response = await fetch(buildPath('api/posts/' + slug), {method: 'GET'})
+            
+            if (response != null)
+            {
+                const json = await response.json();
+    
+                setPost(json);
+
+                setLoading(false);
+            }
+            else
+            {
+                console.log("Response is null");
+            }
+        }
+
+        grabPost();
+    }, [slug]);
+
+    const renderPost = () => {
+        console.log(loading)
+        if (loading) {
+            return <h1>Loading...</h1>
+        }
+        else {
+            return (
+                <>
+                <h1>{ post.Content }</h1>
+                </>
+            )
+        }
+    }
+
+    return (
+        <>
+            <div>
+                {renderPost()}
+            </div>
+        </>
+    )
+
+}

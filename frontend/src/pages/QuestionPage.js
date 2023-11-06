d19436882c2df3ea4a76ac6b8f9591390690ceef
sum244 <91293022+sum244@users.noreply.github.com>
2023-11-06 03:24:23 -0500

    Basic homepage layout

--- frontend/src/pages/QuestionPage.js
+++ frontend/src/pages/QuestionPage.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import GlobalNavbar from '../components/GlobalNavbar';
 import QuestionList from '../components/QuestionList';
+import ChooseAnswer from '../components/ChooseAnswer';
 
 const QuestionPage = () => {
     return (
         <div>
             <GlobalNavbar />
+            <ChooseAnswer />
             <QuestionList />
         </div>
     );

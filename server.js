e91c55aca04aa9999626dc12dcd63386a8c38410
awilliams225 <ashwilliams255@gmail.com>
2023-11-06 20:23:58 -0500

    Put deployment conditions at bottom of server

--- server.js
+++ server.js
@@ -174,26 +174,6 @@ app.post('/api/login', async (req, res, next) =>
   res.status(200).json(ret);
 });
 
-app.listen(PORT, () => 
-{
-  console.log('Server listening on port ' + PORT);
-});
-
-///////////////////////////////////////////////////
-// For Heroku deployment
-
-// Server static assets if in production
-if (process.env.NODE_ENV === 'production') 
-{
-  // Set static folder
-  app.use(express.static('frontend/build'));
-
-  app.get('*', (req, res) => 
- {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
-}
-
 app.post('/api/numQuestions', async (req, res, next) =>
 {
 
@@ -431,5 +411,25 @@ app.post('/api/questions/:pageNum', async (req, res, next) => {
   res.status(200).json(ret);
 
 });
+
+app.listen(PORT, () => 
+{
+  console.log('Server listening on port ' + PORT);
+});
+
+///////////////////////////////////////////////////
+// For Heroku deployment
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') 
+{
+  // Set static folder
+  app.use(express.static('frontend/build'));
+
+  app.get('*', (req, res) => 
+ {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 module.exports = app;

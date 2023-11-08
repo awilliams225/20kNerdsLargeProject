e16511c417d3f39ccacc16958bf130ff80b400bf
Jarod Davies <balancejackfireblade@gmail.com>
2023-11-08 17:50:03 -0500

    Frontend: Fixed URL href bugs

--- frontend/src/components/GlobalNavbar.js
+++ frontend/src/components/GlobalNavbar.js
@@ -9,10 +9,10 @@ export default function GlobalNavbar() {
         <Navbar expand="lg" bg="dark" data-bs-theme="dark">
             <Container>
                 <Nav className="me-auto">
-                    <Nav.Link href="/home">Home</Nav.Link>
+                    <Nav.Link href="/home/">Home</Nav.Link>
                 </Nav>
                 <Nav>
-                    <Nav.Link href="/home">Log Out</Nav.Link>
+                    <Nav.Link href="/home/">Log Out</Nav.Link>
                 </Nav>
             </Container>
         </Navbar>

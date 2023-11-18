2a94adafc379b5f9cd4b94753b554379ac56fc1c
Jarod Davies <balancejackfireblade@gmail.com>
2023-11-17 22:24:13 -0500

    Frontend: Added reply system

---
+++ frontend/src/components/CreateReplyForm.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+
+export default function CreateReplyForm(props) {
+
+    const slug = props.slug;
+
+    const [show, setShow] = useState(false);
+    const [formData, setFormData] = useState({
+        text: ''
+    })
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const handleChange = (event) => {
+        setFormData({...formData, [event.target.name]: event.target.value});
+        console.log(formData);
+    }
+
+    const app_name = 'fight-or-flight-20k-5991cb1c14ef'
+    function buildPath(route) {
+        if (process.env.NODE_ENV === 'production') {
+            return 'https://' + app_name + '.herokuapp.com/' + route;
+        }
+        else {
+            return 'http://localhost:5000/' + route;
+        }
+    }
+
+    const handleSubmit = async () => {
+
+        let userData = localStorage.getItem('user_data');
+        if (!userData) {
+            return;
+        }
+        let userId = JSON.parse(userData).id;
+
+        var obj = { userId: userId, text: formData.text, slug: slug };
+        var js = JSON.stringify(obj);
+
+        const response = await fetch(buildPath('api/addReply'), { method: 'POST', body: js, headers: { 'Content-Type': 'application/json' } });
+
+        if (response != null) {
+            const json = await response.json();
+        }
+        else {
+            console.log("Response is null");
+        }
+    }
+
+    return (
+        <>
+            <Button onClick={handleShow}>
+                Reply
+            </Button>
+            <Modal show={show} data-bs-theme="dark">
+                <Card bg="dark">
+                    <Card.Header>Send reply</Card.Header>
+                    <Card.Body>
+                        <Form className="m-3" onSubmit={handleSubmit}>
+                            <Form.Group className="mb-5" controlId="formText">
+                                <Form.Control name="text" as="textarea" placeholder="Respond with something cool..." onChange={handleChange} />
+                            </Form.Group>
+                            <Button variant="danger me-3" onClick={handleClose}>Cancel</Button>
+                            <Button variant="primary" type="submit">Reply</Button>
+                        </Form>
+                    </Card.Body>
+                </Card>
+            </Modal>
+        </>
+    )
+
+}
--- frontend/src/pages/ReplyPage.js
+++ frontend/src/pages/ReplyPage.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import GlobalNavbar from '../components/GlobalNavbar';
 import Post from '../components/Post';
+import ReplyForum from '../components/ReplyForum';
 
 const ReplyPage = () => {
     return (
         <div>
             <GlobalNavbar />
             <Post />
+            <ReplyForum />
         </div>
     );
 }

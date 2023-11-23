3ceadf801462deb27c3cef4bc4039e9704a1c9b8
awilliams225 <ashwilliams255@gmail.com>
2023-11-22 23:27:33 -0500

    Fixes to previous password validation changes

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,13 +3,9 @@ import ForgotPasswordModal from '../components/ForgotPasswordModal'
 
 export default function Login() {
 
-    var loginName;
-    var loginPassword;
-
     const [message, setMessage] = useState('');
     const [password, setPassword] = useState('');
     const [username, setUsername] = useState('');
-    const [passValid, setPassValid] = useState(false);
 
     const app_name = 'fight-or-flight-20k-5991cb1c14ef'
     function buildPath(route) {
@@ -23,14 +19,6 @@ export default function Login() {
 
     const doLogin = async event => {
         event.preventDefault();
-
-        const validSpan = document.getElementById('passValidSpan');
-
-        if (!passValid)
-        {
-            validSpan.innerHTML = 'This password is invalid!';
-            return;
-        }
 
         var obj = { login: username, password: password };
         var js = JSON.stringify(obj);
@@ -74,60 +62,7 @@ export default function Login() {
     }
 
     const handlePasswordChange = (event) => {
-        var pass = event.target.value;
-
-        const lengthSpan = document.getElementById('passLengthSpan');
-        const upperSpan = document.getElementById('passUpperSpan');
-        const lowerSpan = document.getElementById('passLowerSpan');
-        const numSpan = document.getElementById('passNumberSpan');
-        const specialSpan = document.getElementById('passSpecialSpan');
-
-        const upperCheck = /[A-Z]/g
-        const lowerCheck = /[a-z]/g
-        const numCheck = /[0-9]/g
-        const specialCheck = /[!@#$%^&*]/g
-        const allCheck =  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])[A-Za-z\d@.#$!%*?&]{8,15}$/;
-
-        if (pass.length < 8) {
-            lengthSpan.innerHTML = '* Please make password 8 characters or more';
-            setPassValid(false);
-        }
-        else
-            lengthSpan.innerHTML = '';
-
-        if (!pass.match(upperCheck)) {
-            upperSpan.innerHTML = '* Please include at least 1 uppercase letter';
-            setPassValid(false);
-        }
-        else
-            upperSpan.innerHTML = '';
-
-        if (!pass.match(lowerCheck)) {
-            lowerSpan.innerHTML = '* Please include at least 1 lowercase letter';
-            setPassValid(false);
-        }
-        else
-            lowerSpan.innerHTML = '';
-
-        if (!pass.match(numCheck)) {
-            numSpan.innerHTML = '* Please include at least 1 number';
-            setPassValid(false);
-        }
-        else
-            numSpan.innerHTML = '';        
-
-        if (!pass.match(specialCheck)) {
-            specialSpan.innerHTML = '* Please include at least 1 special character';
-            setPassValid(false);
-        }
-        else
-            specialSpan.innerHTML = '';   
-
-        if (pass.match(allCheck)) {
-            console.log("Hello???");
-            setPassValid(true);
-        }
-
+       setPassword(event.target.value);
     }
 
     return (
@@ -146,12 +81,6 @@ export default function Login() {
                     onClick={doLogin} />
                 <br />
                 <ForgotPasswordModal />
-                <span className='text-light small' id='passValidSpan'></span><br />
-                <span className='text-light small' id='passLengthSpan'></span><br />
-                <span className='text-light small' id='passUpperSpan'></span><br />
-                <span className='text-light small' id='passLowerSpan'></span><br />
-                <span className='text-light small' id='passNumberSpan'></span><br />
-                <span className='text-light small' id='passSpecialSpan'></span><br />
             </form>
             <span id="loginResult">{message}</span>
         </div>

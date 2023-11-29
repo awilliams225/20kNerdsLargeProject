36ae2cc768af3aef00af6de2dd970acfb59f6f24
Jarod Davies <balancejackfireblade@gmail.com>
2023-11-28 19:28:19 -0500

    Frontend: Better timestamps

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -11,7 +11,7 @@ export default function Post(props) {
             <Card className="mb-3 shadow border-5">
                 <Card.Header>
                     <Card.Title>{post.Title}</Card.Title>
-                    Posted by <a href={"/user/" + post.UserId + "/"}>{post.Username}</a> on {new Date(post.Timestamp).toLocaleString()}
+                    Posted by <a href={"/user/" + post.UserId + "/"}>{post.Username}</a> at {new Date(post.Timestamp).toLocaleTimeString()} on {new Date(post.Timestamp).toLocaleDateString()}
                 </Card.Header>
                 <Card.Body>
                     <ReactMarkdown children={post.Content}></ReactMarkdown>

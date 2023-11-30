c30491ee500120dd743c1c3d571a4eb41e6e8a9c
Jarod Davies <balancejackfireblade@gmail.com>
2023-11-30 14:36:27 -0500

    Frontend: fix weird placement

--- frontend/src/components/QuestionForum.js
+++ frontend/src/components/QuestionForum.js
@@ -242,7 +242,7 @@ export default function QuestionForum() {
                 >
                     <span>{ stance.toUpperCase() } MODE</span>
                     
-                    <OverlayTrigger overlay={
+                    <OverlayTrigger placement='bottom' overlay={
                         <Tooltip>
                             <h3>What is this?</h3>
                             This is your <b>stance.</b> In <b style={{color: 'orange'}}>fight</b> mode, you can argue with people about this topic, while in <b style={{color: 'cyan'}}>flight</b> mode, you can talk to other people who feel the same way as you. You cannot change your stance on a question once you answer it, so choose wisely!

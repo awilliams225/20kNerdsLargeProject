99e85abaae23dd9c56d48c969884b5407840a621
Jarod Davies <balancejackfireblade@gmail.com>
2023-11-30 13:00:47 -0500

    Frontend: Gave tooltip nice formatting stuff

--- frontend/src/components/QuestionForum.js
+++ frontend/src/components/QuestionForum.js
@@ -357,7 +357,7 @@ export default function QuestionForum() {
                                 <OverlayTrigger overlay={
                                     <Tooltip>
                                         <h3>What is this?</h3>
-                                        This is your <b>stance.</b> In fight mode, you can argue with people about this topic, while in flight mode, you can talk to other people who feel the same way as you. You cannot change your stance on a question once you answer it, so choose wisely!
+                                        This is your <b>stance.</b> In <b style={{color: 'orange'}}>fight</b> mode, you can argue with people about this topic, while in <b style={{color: 'cyan'}}>flight</b> mode, you can talk to other people who feel the same way as you. You cannot change your stance on a question once you answer it, so choose wisely!
                                     </Tooltip>
                                 }>
                                     <Badge pill bg="secondary" className="m-2">i</Badge>

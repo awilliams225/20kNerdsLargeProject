ac0f5bfbea3d957d51588faab0f026c51835f208
awilliams225 <ashwilliams255@gmail.com>
2023-12-02 19:33:44 -0500

    Raised thickness of answer borders

--- frontend/src/components/QuestionForum.js
+++ frontend/src/components/QuestionForum.js
@@ -326,7 +326,7 @@ export default function QuestionForum() {
                             <ToggleButton className="w-50 d-flex align-items-center justify-content-center"
                                 style={{
                                     height: '40vh', borderRadius: '0', padding: '5vw',
-                                    position: 'relative', zIndex:'0', border: radioValue == 1 ? '5px solid black' : 'none',
+                                    position: 'relative', zIndex:'0', border: radioValue == 1 ? '15px solid black' : 'none',
                                 }}
                                 key={1}
                                 id={"radio-1"}
@@ -343,7 +343,7 @@ export default function QuestionForum() {
                             <ToggleButton className="w-50 d-flex align-items-center justify-content-center"
                                 style={{
                                     height: '40vh', borderRadius: '0', padding: '5vw',
-                                    position: 'relative', zIndex:'0', border: radioValue == 2 ? '5px solid black' : 'none'
+                                    position: 'relative', zIndex:'0', border: radioValue == 2 ? '15px solid black' : 'none'
                                 }}
                                 key={2}
                                 id={"radio-2"}

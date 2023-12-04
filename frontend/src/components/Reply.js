86c2a0b26e87877c574e109b4f7cbfec32c98a55
Jarod Davies <balancejackfireblade@gmail.com>
2023-12-03 21:16:56 -0500

    Frontend: Temp fix for profile reply bug
    Still has weird behavior but at least no longer crashes the page

--- frontend/src/components/Reply.js
+++ frontend/src/components/Reply.js
@@ -2,18 +2,20 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import Card from 'react-bootstrap/Card';
 
-export default function Post(props) {
+export default function Reply(props) {
 
     const reply = props.reply;
 
     return (
         <>
+            {reply !== undefined &&
             <Card>
                 <Card.Body>
                     <Card.Title>{reply.text}</Card.Title>
                     Posted by <a href={`/user/${reply.userId}/`}>{reply.username}</a> at {new Date(reply.timestamp).toLocaleTimeString()}  on {new Date(reply.timestamp).toLocaleDateString()} 
                 </Card.Body>
             </Card>
+            }
         </>
     )
 
